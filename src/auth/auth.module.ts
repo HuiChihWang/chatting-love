83618e3b246b3d1cf75b200365cc3fc161fa839f
Gilbert Wang <taya87136@gmail.com>
2023-07-14 21:39:11 +0800

    remove jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
-import JwtAuthModule from './jwt.module';
 import { JwtRefreshStrategy, JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { CacheModule } from '../cache/cache.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule, JwtAuthModule, PassportModule, CacheModule],
+  imports: [JwtModule.register({}), UserModule, PassportModule, CacheModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, JwtRefreshStrategy],
 })
--- src/auth/jwt.module.ts
+++
@@ -1,9 +0,0 @@
-import { JwtModule } from '@nestjs/jwt';
-
-const JwtAuthModule = JwtModule.register({
-  global: true,
-  secret: 'secret',
-  signOptions: { expiresIn: '1h' },
-});
-
-export default JwtAuthModule;
